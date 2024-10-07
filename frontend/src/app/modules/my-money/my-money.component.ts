615a98586b4cddf60a4dfe725ebae7b345e241b9
Maxim Niko <m.nikohdiuk@autodoc.eu>
2024-10-07 13:45:54 +0300

    mv twa.ready to main module

--- frontend/src/app/modules/my-money/my-money.component.ts
+++ frontend/src/app/modules/my-money/my-money.component.ts
@@ -1,9 +1,10 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterOutlet} from "@angular/router";
 import {HeaderComponent} from "./components/_layout/header/header.component";
 import {FooterComponent} from "./components/_layout/footer/footer.component";
 import {SymbolsComponent} from "../../common/components/symbols/symbols.component";
+import {TwaService} from "../../common/services/twa.service";
 
 @Component({
   standalone: true,
@@ -11,6 +12,12 @@ import {SymbolsComponent} from "../../common/components/symbols/symbols.componen
   templateUrl: './my-money.component.html',
   host: {'class': 'd-flex flex-column h-100'}
 })
-export class MyMoneyComponent {
+export class MyMoneyComponent implements OnInit {
+  constructor(private twa: TwaService) {
+    this.twa.ready()
+  }
 
+  ngOnInit() {
+    this.twa.expand()
+  }
 }
