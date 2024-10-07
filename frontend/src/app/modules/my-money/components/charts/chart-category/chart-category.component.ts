619ee5addac8b74b81397e2a65a5f7b9e059423a
Maxim Niko <m.nikohdiuk@autodoc.eu>
2024-10-07 13:55:55 +0300

    fix routing

--- frontend/src/app/modules/my-money/components/charts/chart-category/chart-category.component.ts
+++ frontend/src/app/modules/my-money/components/charts/chart-category/chart-category.component.ts
@@ -29,8 +29,6 @@ export class ChartCategoryComponent implements OnInit, OnDestroy {
     private activatedRoute: ActivatedRoute,
     private router: Router
   ) {
-    this.goBack = this.goBack.bind(this)
-    this.onMainClick = this.onMainClick.bind(this)
     this.activatedRoute.data
       .subscribe(
         (data: any) => {
@@ -42,16 +40,16 @@ export class ChartCategoryComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.twa.backButtonOnClick(this.goBack)
+    this.twa.backButtonOnClick(() => this.goBack())
     this.twa.setMainButton(
       {text: 'Add transaction', is_active: true, is_visible: true, has_shine_effect: true},
-      this.onMainClick,
+      () => this.onMainClick(),
     )
   }
 
   ngOnDestroy(): void {
-    this.twa.offMainButton(this.onMainClick)
-    this.twa.offBackButton(this.goBack)
+    this.twa.offMainButton(() => this.onMainClick())
+    this.twa.offBackButton(() => this.goBack())
   }
 
   goBack() {
