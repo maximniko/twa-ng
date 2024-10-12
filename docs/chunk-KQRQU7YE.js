9f74cb8a65b7a4a9325d766d2e6020e1ffc9acd7
Maxim Niko <m.nikohdiuk@autodoc.eu>
2024-10-12 16:53:28 +0300

    add settings

---
+++ docs/chunk-KQRQU7YE.js
@@ -0,0 +1,1 @@
+var l={chevronLeft:"chevron-left",chevronRight:"chevron-right",house:"house",sliders:"sliders",chartFill:"chart-fill",menu:"menu",gearFill:"gear-fill",nodePlusFill:"node-plus-fill"};export{l as a};
